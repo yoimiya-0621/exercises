345b58b37236d8ed0e2672785886b70929fca9ba
chenyun-yoimiya <3129404199@qq.com>
2024-01-17 17:17:31 +0800

    前缀和与差分

--- 练习/地毯.cpp
+++ 练习/地毯.cpp
@@ -7,9 +7,9 @@
 //	int a[1001][1001] = { 0 };
 //	int b[1001][1001] = { 0 };
 //	scanf("%d %d", &n, &m);
+//	int x1, y1, x2, y2;
 //	for (int i = 0; i < m; i++)
 //	{
-//		int x1, y1, x2, y2;
 //		scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
 //		for (int j = x1; j <= x2; j++)
 //		{
@@ -19,7 +19,7 @@
 //	}
 //	for (int i = 1; i <= n; i++)
 //	{
-//		for (int j = 1; i <= n; j++)
+//		for (int j = 1; j <= n; j++)
 //		{
 //			a[i][j] = a[i][j - 1] + b[i][j];
 //			printf("%d ", a[i][j]);
