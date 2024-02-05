06b691eefd26b24fc43f399c44f017a575188491
chenyun-yoimiya <3129404199@qq.com>
2024-02-05 16:05:57 +0800

    1

--- 23年_14届蓝桥杯/23年_14届蓝桥杯/冶炼金属.cpp
+++ 23年_14届蓝桥杯/23年_14届蓝桥杯/冶炼金属.cpp
@@ -1,21 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
-
-#include <stdio.h>
-#include <algorithm>
-using namespace std;
-int main()
-{
-	long long a[10000],b[10000];
-	long long v, o,n;
-	scanf("%lld", &n);
-	for (int i = 0; i < n; i++)
-	{
-		scanf("%lld %lld", &v, &o);
-		a[i] = v / (o + 1) + 1;
-		b[i] = v / o;
-	}
-	sort(a, a + n);
-	sort(b, b + n);
-	printf("%lld %lld", a[n - 1], b[0]);
-	return 0;
-}
+//#include <stdio.h>
+//#include <algorithm>
+//using namespace std;
+//int main()
+//{
+//	long long a[10000],b[10000];
+//	long long v, o,n;
+//	scanf("%lld", &n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		scanf("%lld %lld", &v, &o);
+//		a[i] = v / (o + 1) + 1;
+//		b[i] = v / o;
+//	}
+//	sort(a, a + n);
+//	sort(b, b + n);
+//	printf("%lld %lld", a[n - 1], b[0]);
+//	return 0;
+//}
---
+++ 23年_14届蓝桥杯/23年_14届蓝桥杯/飞机降落.cpp
@@ -0,0 +1,89 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <cstdio>
+//#include <algorithm>
+//using namespace std;
+//struct S
+//{
+//	int a;
+//	int b;
+//	int t;
+//}s[10];
+//bool cmp(struct S x, struct S y)
+//{
+//	if (x.a + x.b != y.a + y.b)
+//		return (x.a + x.b) < (y.a + y.b);
+//	else
+//		return x.t < y.t;
+//}
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		int k;
+//		scanf("%d", &k);
+//		for (int j = 0; j < k; j++)
+//		{
+//			scanf("%d %d %d", &s[j].a, &s[j].b, &s[j].t);
+//		}
+//		sort(s, s + k, cmp);
+//		int time = 0,ans=0;
+//		for (int j = 0; j < k; j++)
+//		{
+//			if (time >= s[j].a && time <= (s[j].a + s[j].b))
+//			{
+//				ans++;
+//				time += s[j].t;
+//			}
+//			else
+//			{
+//				printf("NO\n");
+//				break;
+//			}
+//		}
+//		if (ans == k)
+//			printf("YES\n");
+//	}
+//	return 0;
+//}
+#include<iostream>
+#include<cmath>
+#include<queue>
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+const int N = 15;
+int t[N], d[N], l[N];
+int a[N];
+int main()
+{
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		int n;
+		scanf("%d", &n);
+		for (int i = 1; i <= n; i++)
+			scanf("%d%d%d", &t[i], &d[i], &l[i]);
+		for (int i = 1; i <= n; i++)
+			a[i] = i;
+		bool flag = false;
+		do
+		{
+			int now = 0;
+			for (int i = 1; i <= n; i++)
+			{
+				if (now <= t[a[i]] + d[a[i]])
+					now = l[a[i]] + max(now, t[a[i]]);
+				else
+					break;
+				if (i == n) flag = true;
+			}
+			if (flag) break;
+		} while (next_permutation(a + 1, a + n + 1));
+		if (flag) puts("YES");
+		else puts("NO");
+	}
+	return 0;
+}
