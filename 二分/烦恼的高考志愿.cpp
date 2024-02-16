24d924d9b2571bea37ef6356a1e6db4c51ebf695
chenyun-yoimiya <3129404199@qq.com>
2024-02-16 16:01:44 +0800

    1

--- 二分/烦恼的高考志愿.cpp
+++ 二分/烦恼的高考志愿.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <algorithm>
 #include <math.h>
+#include <vector>
 using namespace std;
 //int main()
 //{
@@ -40,12 +41,43 @@ using namespace std;
 //	printf("%d", ans);
 //	return 0;
 //}
+int a[100] = { 0 };
+void print(int n, int* a, int cur)
+{
+	if (cur == n) {
+		for (int i = 1; i <= n; i++) printf("%d ", a[i]);
+		printf("\n");
+	}
+	else 
+		for (int i = 1; i <= n; i++)
+		{
+			int ok = 1;
+			for (int j = 1; j <= cur; j++)
+			{
+				if (a[j] == i)ok = 0;
+				if (ok)
+				{
+					a[cur] = i;
+					print(n, a, cur + 1);
+				}
+			}
+		}
+}
 int main()
 {
-	double a[9] = { 1,2,3,4,5,6,7,8,9 };
-	if (binary_search(a, a + 8, 9))
+	/*if (binary_search(a, a + 8, 9))
 		printf("found");
 	else
-		printf("no found");
+		printf("no found");*/
+	/*vector<int>b[20];
+	int c;
+	scanf("%d", &c);
+	for (int i = 0; i < c; i++)
+	{
+		b[i].push_back(i);
+	}*/
+	int n;
+	scanf("%d", &n);
+	print(n, a, 1);
 	return 0;
 }
