25fe1d7fdb73be3d899bfc77ca1154de361e8066
chenyun-yoimiya <3129404199@qq.com>
2024-02-16 14:25:19 +0800

    1

--- 二分/烦恼的高考志愿.cpp
+++ 二分/烦恼的高考志愿.cpp
@@ -3,40 +3,49 @@
 #include <algorithm>
 #include <math.h>
 using namespace std;
+//int main()
+//{
+//	int m, n,ans=0;
+//	int a[100000] = { 0 };
+//	int b[100000] = { 0 };
+//	scanf("%d %d", &m, &n);
+//	for (int i = 0; i <m; i++)scanf("%d", &a[i]);
+//	sort(a, a + n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		scanf("%d", &b[i]);
+//		int d = 100000;
+//		int l = 0, r = m-1 ;
+//		int mid = (l + r) / 2;
+//		while (l<=r)
+//		{
+//			if (a[mid]< b[i])
+//			{
+//				l = mid+1;
+//			}
+//			if (a[mid] > b[i])
+//			{
+//				r = mid-1;
+//			}
+//			if (a[mid] == b[i])
+//				break;
+//			mid = (l + r) / 2;
+//		}
+//		if (fabs(b[i] - a[l]) < fabs(a[l-1] - b[i]))
+//			d = fabs(b[i] - a[l]);
+//		else
+//			d = fabs(a[l - 1] - b[i]);
+//		ans += d;
+//	}
+//	printf("%d", ans);
+//	return 0;
+//}
 int main()
 {
-	int m, n,ans=0;
-	int a[100000] = { 0 };
-	int b[100000] = { 0 };
-	scanf("%d %d", &m, &n);
-	for (int i = 0; i <m; i++)scanf("%d", &a[i]);
-	sort(a, a + n);
-	for (int i = 0; i < n; i++)
-	{
-		scanf("%d", &b[i]);
-		int d = 100000;
-		int l = 0, r = m-1 ;
-		int mid = (l + r) / 2;
-		while (l<=r)
-		{
-			if (a[mid]< b[i])
-			{
-				l = mid+1;
-			}
-			if (a[mid] > b[i])
-			{
-				r = mid-1;
-			}
-			if (a[mid] == b[i])
-				break;
-			mid = (l + r) / 2;
-		}
-		if (fabs(b[i] - a[l]) < fabs(a[l-1] - b[i]))
-			d = fabs(b[i] - a[l]);
-		else
-			d = fabs(a[l - 1] - b[i]);
-		ans += d;
-	}
-	printf("%d", ans);
+	double a[9] = { 1,2,3,4,5,6,7,8,9 };
+	if (binary_search(a, a + 8, 9))
+		printf("found");
+	else
+		printf("no found");
 	return 0;
 }
