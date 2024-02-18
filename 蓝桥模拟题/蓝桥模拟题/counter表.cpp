c15e51e5d2697412cc1f0c47409d506f9586ca37
chenyun-yoimiya <3129404199@qq.com>
2024-02-18 20:45:31 +0800

    1

---
+++ 蓝桥模拟题/蓝桥模拟题/counter表.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,k=1;
+	cin >> n;
+	while (n>k)
+	{
+		n -= k;
+		k++;
+	}
+	if (k % 2 == 0)
+	{
+		cout << n << "/" << k - (n - 1);
+	}
+	else
+		cout << k-(n-1) << "/" << n;
+	return 0;
+}
--- 蓝桥模拟题/蓝桥模拟题/车站.cpp
+++ 蓝桥模拟题/蓝桥模拟题/车站.cpp
@@ -1,29 +1,29 @@
-#define  _CRT_SECURE_NO_WARNINGS
-#include <cstdio>
-int arr[1000][2];
-int i = 4;
-int j;
-int main()
-{
-	int a, n, m, x,ans=0;
-	scanf("%d %d %d %d", &a, &n, &m, &x);
-	arr[0][0] = { a }; arr[0][1] = { 0 };
-	arr[1][0] = { 0 }; arr[1][1] = { 0 };
-	arr[2][0] = { a }; arr[2][1] = { 0 };
-	arr[3][0] = { 0 }; arr[3][1] = { 1 };
-	int sa = 2*a,sb=1;
-	for (; i < n-1; i++)
-	{
-		arr[i][0] = arr[i - 1][0] + arr[i - 2][0];
-		arr[i][1] = arr[i - 1][1] + arr[i - 2][1];
-		sa += arr[i][0];
-		sb += arr[i][1];
-	}
-	int y = (m - sa)/sb;
-	for (j = 0; j < x; j++)
-	{
-		ans = ans + arr[j][0] + y*arr[j][1];
-	}
-	printf("%d", ans);
-	return 0;
-}
+//#define  _CRT_SECURE_NO_WARNINGS
+//#include <cstdio>
+//int arr[1000][2];
+//int i = 4;
+//int j;
+//int main()
+//{
+//	int a, n, m, x,ans=0;
+//	scanf("%d %d %d %d", &a, &n, &m, &x);
+//	arr[0][0] = { a }; arr[0][1] = { 0 };
+//	arr[1][0] = { 0 }; arr[1][1] = { 0 };
+//	arr[2][0] = { a }; arr[2][1] = { 0 };
+//	arr[3][0] = { 0 }; arr[3][1] = { 1 };
+//	int sa = 2*a,sb=1;
+//	for (; i < n-1; i++)
+//	{
+//		arr[i][0] = arr[i - 1][0] + arr[i - 2][0];
+//		arr[i][1] = arr[i - 1][1] + arr[i - 2][1];
+//		sa += arr[i][0];
+//		sb += arr[i][1];
+//	}
+//	int y = (m - sa)/sb;
+//	for (j = 0; j < x; j++)
+//	{
+//		ans = ans + arr[j][0] + y*arr[j][1];
+//	}
+//	printf("%d", ans);
+//	return 0;
+//}
