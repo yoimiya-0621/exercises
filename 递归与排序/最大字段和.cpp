5d44481b6f2bdc9e2b5c21451ff67c0ae93701f9
chenyun-yoimiya <3129404199@qq.com>
2024-02-26 20:44:36 +0800

    1

--- 递归与排序/最大字段和.cpp
+++ 递归与排序/最大字段和.cpp
@@ -1,21 +1,21 @@
 #define  _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <algorithm>
-using namespace std;
-int a[20001] = { 0 };
-int c[20001] = { 0 };
-int maxn = -88888;
-int main()
-{
-	int n;
-	cin >> n;
-	for (int i = 1; i <= n; i++)
-		cin >> a[i];
-	for (int i = 1; i <= n; i++)
-	{
-		c[i] = max(c[i - 1] + a[i], a[i]);
-		maxn = max(maxn, c[i]);
-	}
-	cout << maxn;
-	return 0;
-}
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//int a[20001] = { 0 };
+//int c[20001] = { 0 };
+//int maxn = -88888;
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	for (int i = 1; i <= n; i++)
+//		cin >> a[i];
+//	for (int i = 1; i <= n; i++)
+//	{
+//		c[i] = max(c[i - 1] + a[i], a[i]);
+//		maxn = max(maxn, c[i]);
+//	}
+//	cout << maxn;
+//	return 0;
+//}
