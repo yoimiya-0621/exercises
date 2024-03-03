b7ead739ccda566a9ea844029ff17ba2fb6e3556
chenyun-yoimiya <3129404199@qq.com>
2024-03-03 21:12:41 +0800

    1

--- 训练1/训练1/2.cpp
+++ 训练1/训练1/2.cpp
@@ -1,105 +1,219 @@
-//#define  _CRT_SECURE_NO_WARNINGS
+////#define  _CRT_SECURE_NO_WARNINGS
+//////#include <cstdio>
+//////int main()
+//////{
+//////	char s[1000] = { 0 };
+//////	char arr[1000] = { 0 };
+//////	scanf("%s", s);
+//////	int flag = 0, m = 0;
+//////	for (int i = 0; s[i] != '\0';i++)
+//////	{
+//////		if (flag == 1)
+//////		{
+//////			if (s[i] >= '1'&& s[i] <='9')
+//////			{
+//////				arr[m]= s[i];
+//////				m++;
+//////			}
+//////		}
+//////		if (s[i] == '(')
+//////			flag = 1;
+//////	}
+//////	if (arr[0] == 0)
+//////		printf("not find");
+//////	else
+//////	printf("%s", arr);
+//////	return 0;
+//////}
+////#include<map>
 ////#include <cstdio>
+////using namespace std;
+////struct S
+////{
+////	char a = 'A';
+////	int b = 0;
+////}c[105];
+//#define  _CRT_SECURE_NO_WARNINGS
+//// 
 ////int main()
 ////{
-////	char s[1000] = { 0 };
-////	char arr[1000] = { 0 };
+////	
+////	char s[105] = { 0 };
 ////	scanf("%s", s);
-////	int flag = 0, m = 0;
-////	for (int i = 0; s[i] != '\0';i++)
+////	int len = strlen(s);
+////	int j = 0;
+////	for (int i = 0; i < len; i++)
 ////	{
-////		if (flag == 1)
+////		int k = 0;
+////		for (k = 0; k <= j; k++)
 ////		{
-////			if (s[i] >= '1'&& s[i] <='9')
-////			{
-////				arr[m]= s[i];
-////				m++;
-////			}
+////			if (c[k].a == s[i] && s[i] >= 'a' && s[i] <= 'z')
+////				c[k].b++;
 ////		}
-////		if (s[i] == '(')
-////			flag = 1;
+////		if (k == j + 1 && s[i] >= 'a' && s[i] <= 'z')
+////		{
+////			c[k].a = s[i];
+////			c[k].b++;
+////			j++;
+////		}
 ////	}
-////	if (arr[0] == 0)
-////		printf("not find");
-////	else
-////	printf("%s", arr);
+////	int flag = 0;
+////	for (int i = 0; i <= j; i++)
+////		if (c[i].b == 1) {
+////			printf("%c", c[i].a);
+////			flag = 1;
+////			break;
+////		}
+////	if (flag == 0)
+////		printf("no");
 ////	return 0;
 ////}
-//#include<map>
-//#include <cstdio>
+////#include <iostream>
+////#include <cstdio>
+////#include <cstring>
+////using namespace std;
+////int main()
+////{
+////	char s[105] = { 0 };
+////	scanf("%s", s);
+////	int len = strlen(s);
+////	for (int i = len - 1; i >= 0; i--)
+////	{
+////		if (s[i] >= 'a' && s[i] <= 'z')
+////		{
+////			s[i] -= 32;
+////			if (s[i] >= 'X' && s[i] <= 'Z')
+////			{
+////				printf("%c", s[i] + 3 - 26);
+////			}
+////			else
+////				printf("%c", s[i] + 3);
+////		}
+////		else
+////		{
+////			s[i] += 32;
+////			if (s[i] >= 'x' && s[i] <= 'z')
+////			{
+////				printf("%c", s[i] + 3 - 26);
+////			}
+////			else
+////				printf("%c", s[i] + 3);
+////		}
+////	}
+////	return 0;
+////}
+//
+//#include  <map>
+//#include <cstring>
+//#include <iostream>
 //using namespace std;
-//struct S
-//{
-//	char a = 'A';
-//	int b = 0;
-//}c[105];
-#define  _CRT_SECURE_NO_WARNINGS
-// 
+//
 //int main()
 //{
-//	
-//	char s[105] = { 0 };
-//	scanf("%s", s);
-//	int len = strlen(s);
-//	int j = 0;
-//	for (int i = 0; i < len; i++)
+//	string s;
+//	cin >> s;
+//	map<char, int>a;
+//	for (int i = 0; i < s.size(); i++)
 //	{
-//		int k = 0;
-//		for (k = 0; k <= j; k++)
-//		{
-//			if (c[k].a == s[i] && s[i] >= 'a' && s[i] <= 'z')
-//				c[k].b++;
-//		}
-//		if (k == j + 1 && s[i] >= 'a' && s[i] <= 'z')
-//		{
-//			c[k].a = s[i];
-//			c[k].b++;
-//			j++;
-//		}
+//		a[s[i]] += 1;
 //	}
-//	int flag = 0;
-//	for (int i = 0; i <= j; i++)
-//		if (c[i].b == 1) {
-//			printf("%c", c[i].a);
-//			flag = 1;
+//	bool flag = false;
+//	for (int i = 0; i < s.size(); i++)
+//	{
+//		if (a[s[i]] == 1)
+//		{
+//			cout << s[i] << "\n";
+//			flag = true;
 //			break;
 //		}
-//	if (flag == 0)
-//		printf("no");
-//	return 0;
-//}
-//#include <iostream>
-//#include <cstdio>
-//#include <cstring>
-//using namespace std;
-//int main()
-//{
-//	char s[105] = { 0 };
-//	scanf("%s", s);
-//	int len = strlen(s);
-//	for (int i = len - 1; i >= 0; i--)
-//	{
-//		if (s[i] >= 'a' && s[i] <= 'z')
-//		{
-//			s[i] -= 32;
-//			if (s[i] >= 'X' && s[i] <= 'Z')
-//			{
-//				printf("%c", s[i] + 3 - 26);
-//			}
-//			else
-//				printf("%c", s[i] + 3);
-//		}
-//		else
-//		{
-//			s[i] += 32;
-//			if (s[i] >= 'x' && s[i] <= 'z')
-//			{
-//				printf("%c", s[i] + 3 - 26);
-//			}
-//			else
-//				printf("%c", s[i] + 3);
-//		}
 //	}
+//	if (!flag)
+//		cout << "no\n";
 //	return 0;
+// 
 //}
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+	string s;
+	while(cin>>s)
+	{
+		while (1) {
+			int mid = s.size() / 2;
+			string a = s.substr(0, mid);
+			string b = s.substr(mid, s.size());
+			reverse(a.begin(), a.end());
+			if (a == b)
+				s = a;
+			else
+				break;
+		}
+		/*int l = 0, r = s.size() - 1;
+		int flag = s.size();
+		while (l < r)
+		{
+			if (l + 1 == r && s[l] == s[r])
+			{
+				l = 0;
+				r--;
+				flag /= 2;
+				continue;
+			}
+			if (s[l] != s[r])
+			{
+				cout << flag<<"\n";
+				break;
+			}
+		}*/
+/*
+		cout << s.size() << "\n";
+	}
+	return 0;
+}*/
 
+#include <iostream>
+using namespace std;
+int p[10] = { 1 },m=0,n;
+void print(int m)
+{
+	int sum = 0;
+	for (int i = 0; i <= m; i++)
+	{
+		sum += p[i];
+	}
+	if (sum == n)
+	{
+		cout << p[0];
+		for (int i = 1; i <= m; i++)
+		{
+			cout << "+" << p[i];
+		}
+		cout << "\n";
+	}
+}
+void dfs(int x)
+{
+	if (x > n)
+		return;
+	for (int i =p[m] ; i <= x; i++)
+	{
+		if (i == n)
+			break;
+		m++;
+		p[m] = i;
+		print(m);
+		dfs(x - i);
+		m --;
+	}
+}
+int main()
+{
+	cin >> n;
+	for (int i = 1; i <= n / 2; i++) {
+		p[0] = i;
+		dfs(n);
+	}
+	return 0;
+}
--- 训练1/训练1/按日期排序.cpp
+++ 训练1/训练1/按日期排序.cpp
@@ -1,135 +1,135 @@
-#define  _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <cstdio>
-#include <algorithm>
-using namespace std;
-struct S
-{
-	int y;
-	char a;
-	int m;
-	char b;
-	int d;
-}a[105];
-bool cmp(S a, S b)
-{
-	if (a.y != b.y)
-		return a.y < b.y;
-	else
-	{
-		if (a.m != b.m)
-			return a.m < b.m;
-		else
-			return a.d < b.d;
-	}
-}
-int main()
-{
-	int n = 0;
-	for (int i = 0; i < 100; i++)
-	{
-		char b[5] = { 0 };
-		scanf("%s", &b);
-		if (b[0] >= 0 && b[0] <= 9)
-		{
-			a[i].m = (b[0] - '0');
-			n++;
-		}
-		else
-			break;
-		scanf("%c%d%c%d",  &a[i].a, &a[i].d, &a[i].b, &a[i].y);
-	}
-	sort(a, a + n, cmp);
-	for (int j = 0; j <= n; j++)
-		cout << a[j].d << "/" << a[j].m << "/" << a[j].y << "\n";
-	return 0;
-}
+//#define  _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
 //#include <cstdio>
-//int main()
+//#include <algorithm>
+//using namespace std;
+//struct S
 //{
-//	char s[25] = { 0 };
-//	scanf("%s", s);
-//	int m = 0, n = 0;
-//	scanf("%d %d", &m, &n);
-//	char arr[25] = { 0 };
-//
-//	for (int i=0; i < n; i++)
+//	int y;
+//	char a;
+//	int m;
+//	char b;
+//	int d;
+//}a[105];
+//bool cmp(S a, S b)
+//{
+//	if (a.y != b.y)
+//		return a.y < b.y;
+//	else
 //	{
-//		arr[i] = s[m];
-//		m++;
+//		if (a.m != b.m)
+//			return a.m < b.m;
+//		else
+//			return a.d < b.d;
 //	}
-//	printf("%s", arr);
-//	return 0;
 //}
-//#include <cstdio>
 //int main()
 //{
-//	int a = 0;
-//	scanf("%d", &a);
-//		int b = a / 100;
-//		a %= 100;
-//		if (b == 1)
-//			printf("Ò¼");
-//		if (b == 2)
-//			printf("·¡");
-//		if (b == 3)
-//			printf("Èþ");
-//		if (b == 4)
-//			printf("ËÁ");
-//		if (b == 5)
-//			printf("Îé");
-//		if (b == 6)
-//			printf("Â½");
-//		if (b == 7)
-//			printf("Æâ");
-//		if (b == 8)
-//			printf("°Æ");
-//		if (b == 9)
-//			printf("¾Á");
-//		if (b == 0)
-//			printf("Áã");
-//		 b = a / 10;
-//		a %= 10;
-//		if (b == 1)
-//			printf("Ò¼");
-//		if (b == 2)
-//			printf("·¡");
-//		if (b == 3)
-//			printf("Èþ");
-//		if (b == 4)
-//			printf("ËÁ");
-//		if (b == 5)
-//			printf("Îé");
-//		if (b == 6)
-//			printf("Â½");
-//		if (b == 7)
-//			printf("Æâ");
-//		if (b == 8)
-//			printf("°Æ");
-//		if (b == 9)
-//			printf("¾Á");
-//		if (b == 0)
-//			printf("Áã");
-//		 b = a;
-//		if (b == 1)
-//			printf("Ò¼");
-//		if (b == 2)
-//			printf("·¡");
-//		if (b == 3)
-//			printf("Èþ");
-//		if (b == 4)
-//			printf("ËÁ");
-//		if (b == 5)
-//			printf("Îé");
-//		if (b == 6)
-//			printf("Â½");
-//		if (b == 7)
-//			printf("Æâ");
-//		if (b == 8)
-//			printf("°Æ");
-//		if (b == 9)
-//			printf("¾Á");
-//		if (b == 0)
-//			printf("Áã");
+//	int n = 0;
+//	for (int i = 0; i < 100; i++)
+//	{
+//		char b[5] = { 0 };
+//		scanf("%s", &b);
+//		if (b[0] >= 0 && b[0] <= 9)
+//		{
+//			a[i].m = (b[0] - '0');
+//			n++;
+//		}
+//		else
+//			break;
+//		scanf("%c%d%c%d",  &a[i].a, &a[i].d, &a[i].b, &a[i].y);
+//	}
+//	sort(a, a + n, cmp);
+//	for (int j = 0; j <= n; j++)
+//		cout << a[j].d << "/" << a[j].m << "/" << a[j].y << "\n";
 //	return 0;
 //}
+////#include <cstdio>
+////int main()
+////{
+////	char s[25] = { 0 };
+////	scanf("%s", s);
+////	int m = 0, n = 0;
+////	scanf("%d %d", &m, &n);
+////	char arr[25] = { 0 };
+////
+////	for (int i=0; i < n; i++)
+////	{
+////		arr[i] = s[m];
+////		m++;
+////	}
+////	printf("%s", arr);
+////	return 0;
+////}
+////#include <cstdio>
+////int main()
+////{
+////	int a = 0;
+////	scanf("%d", &a);
+////		int b = a / 100;
+////		a %= 100;
+////		if (b == 1)
+////			printf("Ò¼");
+////		if (b == 2)
+////			printf("·¡");
+////		if (b == 3)
+////			printf("Èþ");
+////		if (b == 4)
+////			printf("ËÁ");
+////		if (b == 5)
+////			printf("Îé");
+////		if (b == 6)
+////			printf("Â½");
+////		if (b == 7)
+////			printf("Æâ");
+////		if (b == 8)
+////			printf("°Æ");
+////		if (b == 9)
+////			printf("¾Á");
+////		if (b == 0)
+////			printf("Áã");
+////		 b = a / 10;
+////		a %= 10;
+////		if (b == 1)
+////			printf("Ò¼");
+////		if (b == 2)
+////			printf("·¡");
+////		if (b == 3)
+////			printf("Èþ");
+////		if (b == 4)
+////			printf("ËÁ");
+////		if (b == 5)
+////			printf("Îé");
+////		if (b == 6)
+////			printf("Â½");
+////		if (b == 7)
+////			printf("Æâ");
+////		if (b == 8)
+////			printf("°Æ");
+////		if (b == 9)
+////			printf("¾Á");
+////		if (b == 0)
+////			printf("Áã");
+////		 b = a;
+////		if (b == 1)
+////			printf("Ò¼");
+////		if (b == 2)
+////			printf("·¡");
+////		if (b == 3)
+////			printf("Èþ");
+////		if (b == 4)
+////			printf("ËÁ");
+////		if (b == 5)
+////			printf("Îé");
+////		if (b == 6)
+////			printf("Â½");
+////		if (b == 7)
+////			printf("Æâ");
+////		if (b == 8)
+////			printf("°Æ");
+////		if (b == 9)
+////			printf("¾Á");
+////		if (b == 0)
+////			printf("Áã");
+////	return 0;
+////}
