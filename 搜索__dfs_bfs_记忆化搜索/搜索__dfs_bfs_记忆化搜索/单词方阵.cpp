2c79607be321844c1e762732ef70d9efaa1f2c7b
chenyun-yoimiya <3129404199@qq.com>
2024-03-05 21:01:29 +0800

    1

--- 搜索__dfs_bfs_记忆化搜索/搜索__dfs_bfs_记忆化搜索/单词方阵.cpp
+++ 搜索__dfs_bfs_记忆化搜索/搜索__dfs_bfs_记忆化搜索/单词方阵.cpp
@@ -29,74 +29,72 @@
 //	}
 //	return 0;
 //}
-#include <iostream>
-using namespace std;
-char a[100][100] = { 0 };
-char ch[10] = "yizhong";
-int dx[8] = { -1,0,1,-1,1,-1,0,1 };
-int dy[8] = { 1,1,1,0,0,-1,-1,-1 };
-int b[100][3] = { 0 };
-int n, m = 0;
-bool dfs(int x, int y,int k)
-{
-	for (int i = 0; i < 7; i++)
-	{
-		if (x<1 || x>n || y<1 || y>n)
-			return false;
-		if (a[x][y] != ch[i])
-			return false;
-		x += dx[k];
-		y += dy[k];
-	}
-	return true;
-}
-
-int main()
-{
-	cin >> n;
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= n; j++)
-			cin >> a[i][j];
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 1; j <= n; j++)
-		{
-			if (a[i][j] == 'y')
-			{
-				int k = 0;
-				for (k = 0; k < 8; k++)
-				{
-					if (dfs(i, j, k))
-					{
-						b[m][0] = i;
-						b[m][1] = j;
-						b[m][2] = k;
-						m++;
-					}
-				}
-			}
-		}
-	}
-	for (int i = 0; i < m; i++)
-	{
-		for (int j = 0; j < 7; j++)
-		{
-			a[b[i][0]][b[i][1]] = j;
-			j++;
-			b[i][0] += dx[b[i][2]];
-			b[i][1] += dy[b[i][2]];
-		}
-	}
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 1; j <= n; j++)
-		{
-			if (isdigit(a[i][j]))
-				cout << ch[a[i][j]];
-			else
-				cout << "*";
-		}
-		cout << "\n";
-	}
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//char a[105][105] = { 0 };
+//char ch[10] = "yizhong";
+//int dx[8] = { -1,0,1,-1,1,-1,0,1 };
+//int dy[8] = { 1,1,1,0,0,-1,-1,-1 };
+//int b[100][3] = { 0 };
+//int n=0, m = 0;
+//bool dfs(int x, int y,int k)
+//{
+//	for (int i = 0; i < 7; i++)
+//	{
+//		if (x<1 || x>n || y<1 || y>n)
+//			return false;
+//		if (a[x][y] != ch[i])
+//			return false;
+//		x += dx[k];
+//		y += dy[k];
+//	}
+//	return true;
+//}
+//int main()
+//{
+//	cin >> n;
+//	for (int i = 1; i <= n; i++)
+//		for (int j = 1; j <= n; j++)
+//			cin >> a[i][j];
+//	for (int i = 1; i <= n; i++)
+//	{
+//		for (int j = 1; j <= n; j++)
+//		{
+//			if (a[i][j] == 'y')
+//			{
+//				int k = 0;
+//				for (k = 0; k < 8; k++)
+//				{
+//					if (dfs(i, j, k))
+//					{
+//						b[m][0] = i;
+//						b[m][1] = j;
+//						b[m][2] = k;
+//						m++;
+//					}
+//				}
+//			}
+//		}
+//	}
+//	for (int i = 0; i < m; i++)
+//	{
+//		for (int j = 0; j < 7; j++)
+//		{
+//			a[b[i][0]][b[i][1]] = j+'0';
+//			b[i][0] += dx[b[i][2]];
+//			b[i][1] += dy[b[i][2]];
+//		}
+//	}
+//	for (int i = 1; i <= n; i++)
+//	{
+//		for (int j = 1; j <= n; j++)
+//		{
+//			if (isdigit(a[i][j]))
+//				cout << ch[a[i][j]-'0'];
+//			else
+//				cout << "*";
+//		}
+//		cout << "\n";
+//	}
+//	return 0;
+//}
---
+++ 蓝桥模拟题/蓝桥模拟题/14B_2_01串的熵.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//#define N 23333333
+//#define F 11625907.5798
+//double mi = 1000000;
+//int m = 0;
+//double f(double x)
+//{
+//	return -(x * x / N) * log(x / N)/log(2) - (N - x) * (N - x) / N * log((N - x) / N)/log(2);
+//}
+//int main()
+//{
+//	for (double i = 1; i <= N; i++)
+//	{
+//		if (fabs(f(i) - F) < mi)
+//		{
+//			mi = fabs(f(i) - F);
+//			m = i;
+//		}
+//	}
+//	cout << m;
+//	//11027421
+//	return 0;
+//}
