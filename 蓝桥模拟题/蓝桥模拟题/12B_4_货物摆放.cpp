856dfcc40a8442c5e27d6c8790e36e1c5a65c6ef
chenyun-yoimiya <3129404199@qq.com>
2024-03-21 21:18:20 +0800

    1

--- 蓝桥模拟题/蓝桥模拟题/12B_4_货物摆放.cpp
+++ 蓝桥模拟题/蓝桥模拟题/12B_4_货物摆放.cpp
@@ -1,19 +1,4 @@
-////#include <iostream>
-////using namespace std;
-////int main()
-////{
-////	long long ans = 0,a3=0,a2=0;
-////	for (long long i = 1; i <= 2021041820210418; i++)
-////	{
-////		for (long long j = i; j <= 2021041820210418; j++)
-////		{
-////			if (2021041820210418 % i % j == 0)
-////				ans++;
-////		}
-////	}
-////	cout << ans;
-////	return 0;
-////}
+
 //#include <iostream>
 //#include <cmath>
 //using namespace std;
