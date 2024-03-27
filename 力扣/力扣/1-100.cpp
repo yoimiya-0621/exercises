f899ff65e0c8fa20da0a29fb2d261aa1ab0a1581
chenyun-yoimiya <3129404199@qq.com>
2024-03-27 21:01:18 +0800

    力扣第二天

--- 力扣/力扣/1-100.cpp
+++ 力扣/力扣/1-100.cpp
@@ -35,3 +35,83 @@
 //        return ans;
 //    }
 //};
+//5.最长子回文串
+//class Solution {
+//public:
+//    string longestPalindrome(string s) {
+//        int n = 0;
+//        string ans;
+//        for (int i = 0; i < s.length(); i++)
+//        {
+//            int l = i, r = i;
+//            while (1) {
+//                if (l<0 || r>s.length())
+//                    break;
+//                if (s[l] != s[r])
+//                    break;
+//                l--;
+//                r++;
+//            }
+//            r--;
+//            l++;
+//            if (n < r - l + 1 && r < s.length() && l >= 0 && l <= r)
+//            {
+//                n = r - l;
+//                ans = s.substr(l, r - l + 1);
+//            }
+//            int a = i, b = i + 1;
+//            while (1) {
+//                if (a<0 || b>s.length())
+//                    break;
+//                if (s[a] != s[b])
+//                    break;
+//                a--;
+//                b++;
+//            }
+//            a++;
+//            b--;
+//            if (n < b - a + 1 && a >= 0 && b < s.length() && a < b)
+//            {
+//                n = b - a + 1;
+//                ans = s.substr(a, b - a + 1);
+//            }
+//        }
+//        return ans;
+//    }
+//};
+
+//6.Z字形变换
+//class Solution {
+//public:
+//    string convert(string s, int numRows) {
+//        string ans;
+//        if (numRows == 1)
+//            return s;
+//        int e = 2 * numRows - 2;
+//        int m = (s.length() / e) * (numRows - 1);
+//        if (s.length() % e != 0)
+//            m += 1;
+//        if (s.length() % e > numRows)
+//            m += s.length() % e - numRows;
+//        for (int i = 1; i <= numRows; i++)
+//        {
+//            int flag = 0;
+//            for (int j = 1; j <= m; j++)
+//            {
+//                if ((j - 1) % (numRows - 1) == 0) {
+//                    flag++;
+//                    if (i + (flag - 1) * e - 1 < s.length())
+//                        // if(isalpha(s[i+(flag-1)*e-1]))
+//                        ans.push_back(s[i + (flag - 1) * e - 1]);
+//                }
+//                else if ((j - 1) % (numRows - 1) + i == numRows)
+//                {
+//                    if (e * (flag - 1) + numRows + (j - 1) % (numRows - 1) - 1 < s.length())
+//                        //if(isalpha(s[e*(flag-1)+numRows+(j-1)%(numRows-1)-1]))
+//                        ans.push_back(s[e * (flag - 1) + numRows + (j - 1) % (numRows - 1) - 1]);
+//                }
+//            }
+//        }
+//        return ans;
+//    }
+//};
--- 训练1/训练1/天梯赛训练.cpp
+++ 训练1/训练1/天梯赛训练.cpp
@@ -152,42 +152,104 @@ int main()
 //	BB bb;
 //	return 0;
 //}
-#include <iostream>
-using namespace std;
-class A {
-public:
-	A() {
-		cout << "调用A的构造函数" << "\n";
-	}
-	
-private:
-	int a;
-protected:
-	void inia(int x) {
-		a = x;
-		cout << "a:" << a << "\n";
-	}
-};
-class B :public A {
-public:
-	B() {
-		cout << "调用B的构造函数" << "\n";
-	}
-	
-private:
-	int b;
-	
-protected:
-	int xb;
-	void inib(int x) {
-		b = x;
-		cout << "b:" << b << "\n";
-	}
-};
+//#include <iostream>
+//using namespace std;
+//class A {
+//public:
+//	A() {
+//		cout << "调用A的构造函数" << "\n";
+//	}
+//	
+//private:
+//	int a;
+//protected:
+//	void inia(int x) {
+//		a = x;
+//		cout << "a:" << a << "\n";
+//	}
+//};
+//class B :public A {
+//public:
+//	B() {
+//		cout << "调用B的构造函数" << "\n";
+//	}
+//	
+//private:
+//	int b;
+//	
+//protected:
+//	int xb;
+//	void inib(int x) {
+//		b = x;
+//		cout << "b:" << b << "\n";
+//	}
+//};
+//
+//int main()
+//{
+//	B b;
+//	b.inibb(2);
+//	return 0;
+//}
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//int main()
+//{
+//	double sum = 0;
+//	for (int i = 11; i <= 30; i++)
+//	{
+//		sum += pow(1.05, i) * 5000;
+//	}
+//		printf("%.6lf", sum);
+//	return 0;
+//}
 
-int main()
-{
-	B b;
-	b.inibb(2);
+//#include  <iostream>
+//using namespace std;
+//int main()
+//{
+//	//钱总数sum
+//	double sum = 0;
+//	for (int i = 1; i <= 30; i++)//存放三十年
+//	{
+//		//年初，1-20年年初存入5000元
+//		if (i >= 1 && i <= 20)
+//		{
+//			sum += 5000;
+//		}
+//		//年末
+//		sum *= 1.05;
+//	}
+//	printf("%.6lf", sum);
+//	return 0;
+// }
+#include<iostream>
+using namespace std;
+class Box {
+public:
+	Box(int l = 0, int w = 0, int h = 0);
+	int area();
+private:
+	int length;
+	int width;
+	int high;
+};
+Box::Box(int l, int w , int h) {
+	length = l;
+	width = w;
+	high = h;
+
+}
+int Box::area() {
+	return length * width * high;
+}
+int main() {
+	int length;
+	int width;
+	int high;
+	cin >> length >> width >>  high ;
+	Box b(length, width, high);
+	cout << b.area() << endl;
 	return 0;
 }
