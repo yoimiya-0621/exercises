ad48d57f144f7f5d1904d79e2f71bfb795d2e7fd
chenyun-yoimiya <3129404199@qq.com>
2024-04-10 21:00:04 +0800

    1

--- 搜索__dfs_bfs_记忆化搜索/搜索__dfs_bfs_记忆化搜索/花生采摘.cpp
+++ 搜索__dfs_bfs_记忆化搜索/搜索__dfs_bfs_记忆化搜索/花生采摘.cpp
@@ -1,51 +1,75 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <cmath>
+//using namespace std;
+//int a[25][25] = { 0 };
+//int m, n,t,k=0;
+//struct S
+//{
+//	int x;
+//	int y;
+//	int num;
+//}s[200];
+//bool cmp(S a, S b)
+//{
+//	return a.num > b.num;
+//}
+//int main()
+//{
+//	cin >> n >> m >> t;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		for (int j = 1; j <= m; j++)
+//		{
+//			cin >> a[i][j];
+//			if (a[i][j])
+//			{
+//				s[k].x = i;
+//				s[k].y = j;
+//				s[k].num = a[i][j];
+//				k++;
+//			}
+//		}
+//	}
+//	sort(s, s + k, cmp);
+//	int i = 0,j=s[0].y, time = 0, ans = 0, kk = 0;
+//	while (1)
+//	{
+//		if (time +1+ fabs(s[kk].x - i) + fabs(s[kk].y - j) + s[kk].x <= t)
+//		{
+//			time += fabs(s[kk].x - i) + fabs(s[kk].y - j)+1;
+//			i = s[kk].x;
+//			j = s[kk].y;
+//			ans += s[kk].num;
+//			kk++;
+//		}
+//		else
+//			break;
+//	}
+//	cout << ans;
+//	return 0;
+//}
+
 #include <iostream>
 #include <algorithm>
-#include <cmath>
 using namespace std;
-int a[25][25] = { 0 };
-int m, n,t,k=0;
-struct S
-{
-	int x;
-	int y;
-	int num;
-}s[200];
-bool cmp(S a, S b)
-{
-	return a.num > b.num;
-}
+int n;
+int dp[20005][2] = { 0 };
+int l[20005] = { 0 }, r[20005] = { 0 };
 int main()
 {
-	cin >> n >> m >> t;
+	cin >> n;
 	for (int i = 1; i <= n; i++)
 	{
-		for (int j = 1; j <= m; j++)
-		{
-			cin >> a[i][j];
-			if (a[i][j])
-			{
-				s[k].x = i;
-				s[k].y = j;
-				s[k].num = a[i][j];
-				k++;
-			}
-		}
+		cin >> l[i] >> r[i];
 	}
-	sort(s, s + k, cmp);
-	int i = 0,j=s[0].y, time = 0, ans = 0, kk = 0;
-	while (1)
+	dp[1][0] = r[1]-1 + r[1] - l[1];
+	dp[1][1] = r[1]-1;
+	for (int i = 2; i <= n; i++)
 	{
-		if (time +1+ fabs(s[kk].x - i) + fabs(s[kk].y - j) + s[kk].x <= t)
-		{
-			time += fabs(s[kk].x - i) + fabs(s[kk].y - j)+1;
-			i = s[kk].x;
-			j = s[kk].y;
-			ans += s[kk].num;
-			kk++;
-		}
-		else
-			break;
+		dp[i][0] = min(dp[i - 1][0] + abs(r[i] - l[i - 1]) + r[i] - l[i], dp[i - 1][1] + abs(r[i-1] - r[i])+r[i]-l[i]);
+		dp[i][1] = min(dp[i - 1][0] + abs(l[i] - l[i - 1]) + r[i] - l[i], dp[i - 1][1] + abs(r[i - 1] - l[i]) + r[i] - l[i]);
 	}
-	cout << ans;
+	cout << min(dp[n][0]+n-l[n], dp[n][1]+n-r[n]) + n - 1;
 	return 0;
 }
--- 蓝桥模拟题/蓝桥模拟题/13_A_4_选数异或.cpp
+++ 蓝桥模拟题/蓝桥模拟题/13_A_4_选数异或.cpp
@@ -1,35 +1,63 @@
+//#include <iostream>
+//#include <map>
+//using namespace std;
+//#define ll long long
+//int main()
+//{
+//	int n,m,x;
+//	int a[100005] = { 0 };
+//	cin >> n>>m>>x;
+//	for (int i = 1; i <= n; i++)
+//		cin >> a[i];
+//	int l, r;
+//	while (m--)
+//	{
+//		cin >> l >> r;
+//		map<ll, int>ans;
+//		int flag = 0;
+//		for (int i = l; i <= r; i++)
+//		{
+//			ans[a[i]] = 1;
+//		}
+//		for (int i = l; i <= r; i++)
+//		{
+//			if (ans[x ^ a[i]] == 1)
+//			{
+//				flag = 1;
+//				cout << "yes" << "\n";
+//				break;
+//			}
+//		}
+//		if (!flag)
+//			cout << "no" << "\n";
+//	}
+//	return 0;
+//}
 #include <iostream>
-#include <map>
+#include <algorithm>
 using namespace std;
 #define ll long long
+ll dp[100010] = { 0 };
+ll last[100010] = { 0 };
+ll n, m, x, a;
 int main()
 {
-	int n,m,x;
-	int a[100005] = { 0 };
-	cin >> n>>m>>x;
+	cin >> n >> m >> x;
 	for (int i = 1; i <= n; i++)
-		cin >> a[i];
+	{
+		cin >> a;
+		dp[i] = max(dp[i - 1], last[a ^ x]);
+		last[a] = i;
+	}
 	int l, r;
 	while (m--)
 	{
 		cin >> l >> r;
-		map<ll, int>ans;
-		int flag = 0;
-		for (int i = l; i <= r; i++)
-		{
-			ans[a[i]] = 1;
-		}
-		for (int i = l; i <= r; i++)
-		{
-			if (ans[x ^ a[i]] == 1)
-			{
-				flag = 1;
-				cout << "yes" << "\n";
-				break;
-			}
-		}
-		if (!flag)
-			cout << "no" << "\n";
+		if (dp[r] >= l)
+			cout << "yes";
+		else
+			cout << "no";
+		cout << "\n";
 	}
 	return 0;
 }
