199fe17c50070aab819b986b18b79b618a1de2bd
chenyun-yoimiya <3129404199@qq.com>
2024-04-11 20:48:32 +0800

    1

--- 蓝桥模拟题/蓝桥模拟题/13_A_4_选数异或.cpp
+++ 蓝桥模拟题/蓝桥模拟题/13_A_4_选数异或.cpp
@@ -33,31 +33,82 @@
 //	}
 //	return 0;
 //}
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//#define ll long long
+//ll dp[100010] = { 0 };
+//ll last[100010] = { 0 };
+//ll n, m, x, a;
+//int main()
+//{
+//	cin >> n >> m >> x;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		cin >> a;
+//		dp[i] = max(dp[i - 1], last[a ^ x]);
+//		last[a] = i;
+//	}
+//	int l, r;
+//	while (m--)
+//	{
+//		cin >> l >> r;
+//		if (dp[r] >= l)
+//			cout << "yes";
+//		else
+//			cout << "no";
+//		cout << "\n";
+//	}
+//	return 0;
+//}
+
 #include <iostream>
 #include <algorithm>
+#include <map>
 using namespace std;
-#define ll long long
-ll dp[100010] = { 0 };
-ll last[100010] = { 0 };
-ll n, m, x, a;
+long long n, m, mi = 500000;
+struct C {
+	int a;
+	int b;
+}c[200005];
+bool cmp(C a, C b)
+{
+	if (a.a != b.a)
+		return a.a < b.a;
+	else
+		return a.b < b.b;
+}
+map<int, long long>ma;
 int main()
 {
-	cin >> n >> m >> x;
-	for (int i = 1; i <= n; i++)
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> c[i].a;
+	for (int i = 0; i < n; i++)
 	{
-		cin >> a;
-		dp[i] = max(dp[i - 1], last[a ^ x]);
-		last[a] = i;
+		cin >> c[i].b;
+		ma[i] = c[i].b + c[i].a;
+		mi = min(mi, ma[i]);
 	}
-	int l, r;
-	while (m--)
-	{
-		cin >> l >> r;
-		if (dp[r] >= l)
-			cout << "yes";
+	sort(c, c + n, cmp);
+	long long sum = 0, i = 0, ans = c[0].a;
+	while (i + 1 < n) {
+		if (c[i + 1].a - c[i].a <= c[i].b)
+		{
+			sum += (c[i + 1].a - c[i].a) * (i + 1);
+			if (sum > m)
+				break;
+			ans = c[i + 1].a;
+			if (ans > mi)
+			{
+				ans = mi;
+				break;
+			}
+		}
 		else
-			cout << "no";
-		cout << "\n";
+			break;
+		i++;
 	}
+	cout << ans;
 	return 0;
 }
