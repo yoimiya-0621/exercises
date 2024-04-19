c05c8f4dea35a99a05278387cdcfa667fc2fe208
chenyun-yoimiya <3129404199@qq.com>
2024-04-19 21:15:05 +0800

    1

--- 训练/训练1/期中考试.cpp
+++ 训练/训练1/期中考试.cpp
@@ -1,17 +1,37 @@
-#include <iostream>
-using namespace std;
+//#include <iostream>
+//using namespace std;
 //class Sample
 //{
 //public:
 //	Sample();
 //	Sample(int x);
 //};
-class A {
-
-};
+//class A {
+//
+//};
+//int main()
+//{
+//	A* a = new A(10);
+//	
+//	return 0;
+//}
+#include <iostream>
+using namespace std;
 int main()
 {
-	A* a = new A(10);
-	
+	int n;
+	cin >> n;
+	int flag = 0;
+	for (int i = 1; i <= n/2; i++)
+	{
+		if (i % 2 == 0 && (n - i) % 2 == 0) {
+			flag = 1;
+			break;
+		}
+	}
+	if (flag)
+		cout << "YES";
+	else
+		cout << "NO";
 	return 0;
 }
