14a16feb1e1b061780135e58a33b17b4432abc0a
chenyun-yoimiya <3129404199@qq.com>
2024-05-24 21:04:54 +0800

    1

--- 牛客周赛/牛客周赛/24_5_19_Round43.cpp
+++ 牛客周赛/牛客周赛/24_5_19_Round43.cpp
@@ -155,108 +155,108 @@
 ////	cout << -1;
 ////	return 0;
 ////}
-#include <iostream>
-#include <vector>
-#include <map>
-#include <string>
-using namespace std;
-int main()
-{
-    int n, m, t = 0;
-    cin >> n;
-    map<string, int>ok;
-    map<string, string>ans;
-    string xxx;
-    vector<string>s;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> t;
-        getline(cin, xxx);
-        s.push_back(xxx);
-        string str;
-
-        for (int j = 0; j < s[i].size(); j++)
-        {
-            if (j == 0 || s[i][j - 1] == ' ' && isalpha(s[i][j]))
-                str += s[i][j];
-        }
-        ans[s[i]] = str;
-        ok[s[i]] = t;
-    }
-    cin >> m;
-    char c = getchar();
-    string x, y;
-    while (m--)
-    {
-        int c = 0, zheng = 0, fan = 0;
-        string str1, str2, ans1, ans2;
-        vector<string>vv;
-        map<char, int>mp;
-        getline(cin, x);
-        getline(cin, y);
-        for (int j = 0; j < x.size(); j++)
-        {
-            mp[x[j]] = 1;
-            if (j == 0 || x[j - 1] == ' ' && isalpha(x[j]))
-                str1 += x[j];
-        }
-        for (int j = 0; j < y.size(); j++)
-        {
-            mp[y[j]] = 1;
-            if (j == 0 || y[j - 1] == ' ' && isalpha(y[j]))
-                str2 += y[j];
-        }
-        ans1 = str1 + str2;
-        ans2 = str2 + str1;
-        for (int j = 0; j < n; j++)
-        {
-            if (ans["s[j]"] == ans1 || ans["s[j]"] == ans2)
-            {
-                vv.push_back(s[j]);
-                if (ok[s[j]] == 1)
-                    zheng++;
-                else
-                    fan++;
-                c++;
-            }
-        }
-        int k = 0;
-        int h = 0;
-
-        if (c == 0)
-        {
-            cout << "tian ji bu ke xie lu" << '\n';
-            continue;
-        }
-        if (zheng != 0)
-        {
-            k = mp.size() % zheng + 1;
-            for (int j = 0; j < vv.size(); j++)
-            {
-                if (ok[vv[j]] == 1)
-                    h++;
-                if (h == k)
-                {
-                    cout << vv[j] << '\n';
-                    break;
-                }
-            }
-        }
-        else
-        {
-            k = mp.size() % fan + 1;
-            for (int j = 0; j < vv.size(); j++)
-            {
-                if (ok[vv[j]] == 0)
-                    h++;
-                if (h == k)
-                {
-                    cout << vv[j] << '\n';
-                    break;
-                }
-            }
-        }
-
-    }
-    return 0;
-}
+//#include <iostream>
+//#include <vector>
+//#include <map>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//    int n, m, t = 0;
+//    cin >> n;
+//    map<string, int>ok;
+//    map<string, string>ans;
+//    string xxx;
+//    vector<string>s;
+//    for (int i = 0; i < n; i++)
+//    {
+//        cin >> t;
+//        getline(cin, xxx);
+//        s.push_back(xxx);
+//        string str;
+//
+//        for (int j = 0; j < s[i].size(); j++)
+//        {
+//            if (j == 0 || s[i][j - 1] == ' ' && isalpha(s[i][j]))
+//                str += s[i][j];
+//        }
+//        ans[s[i]] = str;
+//        ok[s[i]] = t;
+//    }
+//    cin >> m;
+//    char c = getchar();
+//    string x, y;
+//    while (m--)
+//    {
+//        int c = 0, zheng = 0, fan = 0;
+//        string str1, str2, ans1, ans2;
+//        vector<string>vv;
+//        map<char, int>mp;
+//        getline(cin, x);
+//        getline(cin, y);
+//        for (int j = 0; j < x.size(); j++)
+//        {
+//            mp[x[j]] = 1;
+//            if (j == 0 || x[j - 1] == ' ' && isalpha(x[j]))
+//                str1 += x[j];
+//        }
+//        for (int j = 0; j < y.size(); j++)
+//        {
+//            mp[y[j]] = 1;
+//            if (j == 0 || y[j - 1] == ' ' && isalpha(y[j]))
+//                str2 += y[j];
+//        }
+//        ans1 = str1 + str2;
+//        ans2 = str2 + str1;
+//        for (int j = 0; j < n; j++)
+//        {
+//            if (ans["s[j]"] == ans1 || ans["s[j]"] == ans2)
+//            {
+//                vv.push_back(s[j]);
+//                if (ok[s[j]] == 1)
+//                    zheng++;
+//                else
+//                    fan++;
+//                c++;
+//            }
+//        }
+//        int k = 0;
+//        int h = 0;
+//
+//        if (c == 0)
+//        {
+//            cout << "tian ji bu ke xie lu" << '\n';
+//            continue;
+//        }
+//        if (zheng != 0)
+//        {
+//            k = mp.size() % zheng + 1;
+//            for (int j = 0; j < vv.size(); j++)
+//            {
+//                if (ok[vv[j]] == 1)
+//                    h++;
+//                if (h == k)
+//                {
+//                    cout << vv[j] << '\n';
+//                    break;
+//                }
+//            }
+//        }
+//        else
+//        {
+//            k = mp.size() % fan + 1;
+//            for (int j = 0; j < vv.size(); j++)
+//            {
+//                if (ok[vv[j]] == 0)
+//                    h++;
+//                if (h == k)
+//                {
+//                    cout << vv[j] << '\n';
+//                    break;
+//                }
+//            }
+//        }
+//
+//    }
+//    return 0;
+//}
---
+++ 牛客周赛/牛客周赛/24_5_24.cpp
@@ -0,0 +1,111 @@
+////A
+//#include <iostream>
+//#include <map>
+//using namespace std;
+//int main()
+//{
+//	string s;
+//	map<int, int>mp;
+//	int x = 0;
+//	for (int i = 1; i <= 9; i++)
+//	{
+//		cin >> x;
+//		mp[i] = x;
+//	}
+//	cin >> s;
+//	for (int i = 0; i < s.size(); i++)
+//	{
+//		cout << mp[s[i]-'0'];
+//	}
+//	return 0;
+//}
+//B
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	int a[200010] = { 0 };
+//	int m = 0,flag=0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> a[i];
+//		if (i)
+//		{
+//			m = max(m, a[i - 1]);
+//			if (a[i] < m)
+//			{
+//				flag = 1;
+//			}
+//		}
+//	}
+//	if (flag)
+//		cout << n;
+//	else
+//		cout << 0;
+//	return 0;
+//}
+//C
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//#define ll long long
+//ll a[200010] = { 0 };
+//ll b[200010] = { 0 };
+//ll c[200010] = { 1e9 };
+//ll d[200010] = { 0 };
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	ll ans = 0;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		cin >> a[i];
+//		b[i] = b[i - 1] + a[i];
+//		c[i] = min(a[i], c[i - 1]);
+//	}
+//	for (int i = 1; i <= n; i++)
+//	{
+//		ans = max(ans, b[n] - b[i] - c[i]);
+//	}
+//	for (int i = n; i >=1 ; i--)
+//	{
+//		d[n] = a[i];
+//		if (i != n)
+//		{
+//			d[i] = min(a[i], d[i + 1]);
+//		}
+//		ans = max(ans, b[i-1] - d[i]);
+//	}
+//	cout << ans;
+//	return 0;
+//}
+//D
+#include <iostream>
+#include <algorithm>
+using namespace std;
+const int N = 2e5 + 10;
+int a[N];
+int b[N];
+int main()
+{
+	int n, flag = 0;;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+		if (i)
+		{
+			if (a[i - 1] % a[i] != 0)
+				flag = 1;
+
+		}
+	}
+	if (flag)
+		cout << -1;
+	return 0;
+}
