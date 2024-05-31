4e9b1b53976da47e95bef32efb603094a8e4d760
chenyun-yoimiya <3129404199@qq.com>
2024-05-31 20:33:17 +0800

    1

--- 牛客周赛/牛客周赛/24_5_31.cpp
+++ 牛客周赛/牛客周赛/24_5_31.cpp
@@ -13,6 +13,7 @@
 //        cout << 'b';
 //    return 0;
 //}
+
 //B
 //#include <iostream>
 //using namespace std;
@@ -27,44 +28,45 @@
 //	return 0;
 //}
 
-#include <iostream>
-#include <vector>
-#include <map>
-#include <algorithm>
-using namespace std;
-const int N = 5e5 + 5;
-int a[N];
-int main()
-{
-	int n;
-	cin >> n;
-	vector<vector<int>>mp(2);
-	for (int i = 1; i <= n; i++)
-	{
-		cin >> a[i];
-		mp[a[i]].push_back(i);
-	}
-	if (a[1] == a[n])
-		cout << 1;
-	else {
-		if (a[1] == 0) {
-			for (int i = 1; i < mp[0].size(); i++) {
-				if (binary_search(mp[1].begin(), mp[1].end(), mp[0][i] + 1) && mp[1][mp[1].size() - 1] != mp[0][i] + 1) {
-					cout << 2;
-					return 0;
-				}
-			}
-			cout << -1;
-		}
-		else {
-			for (int i = 1; i < mp[1].size(); i++) {
-				if (binary_search(mp[0].begin(),mp[0].end(),mp[1][i]+1)&&mp[0][mp[0].size()-1]!= mp[1][i] + 1) {
-					cout << 2;
-					return 0;
-				}
-			}
-			cout << -1;
-		}
-	}
-	return 0;
-}
+//C
+//#include <iostream>
+//#include <vector>
+//#include <map>
+//#include <algorithm>
+//using namespace std;
+//const int N = 5e5 + 5;
+//int a[N];
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	vector<vector<int>>mp(2);
+//	for (int i = 1; i <= n; i++)
+//	{
+//		cin >> a[i];
+//		mp[a[i]].push_back(i);
+//	}
+//	if (a[1] == a[n])
+//		cout << 1;
+//	else {
+//		if (a[1] == 0) {
+//			for (int i = 1; i < mp[0].size(); i++) {
+//				if (binary_search(mp[1].begin(), mp[1].end(), mp[0][i] + 1) && mp[1][mp[1].size() - 1] != mp[0][i] + 1) {
+//					cout << 2;
+//					return 0;
+//				}
+//			}
+//			cout << -1;
+//		}
+//		else {
+//			for (int i = 1; i < mp[1].size(); i++) {
+//				if (binary_search(mp[0].begin(),mp[0].end(),mp[1][i]+1)&&mp[0][mp[0].size()-1]!= mp[1][i] + 1) {
+//					cout << 2;
+//					return 0;
+//				}
+//			}
+//			cout << -1;
+//		}
+//	}
+//	return 0;
+//}
