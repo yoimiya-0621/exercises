007aab7e83aca5bb0e1feb04c80b09669c5925de
chenyun-yoimiya <3129404199@qq.com>
2024-07-23 17:39:08 +0800

    牛客

--- 牛客周赛/牛客周赛/24_7_14_Round51.cpp
+++ 牛客周赛/牛客周赛/24_7_14_Round51.cpp
@@ -373,45 +373,116 @@
 //    return 0;
 //}
 
+//#include <iostream>
+//#include <vector>
+//#include <map>
+//using namespace std;
+//#define ll long long
+//const int N = 2e5 + 10;
+//int n, x, y;
+//int dx[4] = { 0,1,0,-1 };
+//int dy[4] = { 1,0,-1,0 };
+//ll ans = 0;
+//vector<pair<int, int>>v;
+//map<pair<int, int>, int>mp;
+//int main()
+//{
+//    string s;
+//    cin >> n >> x >> y >> s;
+//    pair<int, int>v0(0, 0);
+//    for (int i = 0; i < n; i++) {
+//        if (s[i] == 'W') {
+//            v0.second += 1;
+//        }
+//        else if (s[i] == 'S') {
+//            v0.second -= 1;
+//        }
+//        else if (s[i] == 'A') {
+//            v0.first -= 1;
+//        }
+//        else {
+//            v0.first += 1;
+//        }
+//        v.push_back(v0);
+//    }
+//    for (int i = n - 1; i >= 0; i--) {
+//        mp[v[i]] = i + 1;
+//        pair<int,int> ok(v[i].first + x, v[i].second + y);
+//        if (mp[ok]) {
+//            ans += n - mp[ok] + 1;
+//        }
+//    }
+//    cout << ans;
+//    return 0;
+//}
+
 #include <iostream>
-#include <vector>
 #include <map>
+#include <vector>
 using namespace std;
-#define ll long long
-const int N = 2e5 + 10;
-int n, x, y;
-int dx[4] = { 0,1,0,-1 };
-int dy[4] = { 1,0,-1,0 };
-ll ans = 0;
-vector<pair<int, int>>v;
-map<pair<int, int>, int>mp;
-int main()
-{
+int a, n, b;
+int wina, winb;
+int WINA, WINB;
+map<int, int>mp;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     string s;
-    cin >> n >> x >> y >> s;
-    pair<int, int>v0(0, 0);
+    cin >> n >> a >> b;
+    cin >> s;
+    int flag = 0, k = 0;
+    map<int, int>mp1;
+    map<int, int>mp2;
+    for (int i = 0; i < n; i++)
+    {
+        wina = 0;
+        winb = 0;
+        for (int j = i;; j++)
+        {
+            j %= n;
+            if (s[j] == '1')
+                wina++;
+            else
+                winb++;
+            if (wina >= a) {
+                mp1[i] = j+1;
+                mp2[i] = 2;
+                break;
+            }
+            else if (winb >= a) {
+                mp1[i] = j+1;
+                mp2[i] = 1;
+                break;
+            }
+        }
+        
+    }
     for (int i = 0; i < n; i++) {
-        if (s[i] == 'W') {
-            v0.second += 1;
+        WINA = 0;
+        WINB = 0;
+        wina = 0;
+        winb = 0;
+        for (int j = i;;)
+        {
+            j %= n;
+            if (mp1[j]) {
+                if (mp2[j] == 2)
+                    WINA++;
+                else if (mp2[j] == 1)
+                    WINB++;
+                j = mp1[j];
+            }
+            if (WINA >= b) {
+                flag = 1;
+                break;
+            }
+            if (WINB >= b) {
+                flag = 0;
+                break;
+            }
         }
-        else if (s[i] == 'S') {
-            v0.second -= 1;
-        }
-        else if (s[i] == 'A') {
-            v0.first -= 1;
-        }
-        else {
-            v0.first += 1;
-        }
-        v.push_back(v0);
+        cout << flag;
     }
-    for (int i = n - 1; i >= 0; i--) {
-        mp[v[i]] = i + 1;
-        pair<int,int> ok(v[i].first + x, v[i].second + y);
-        if (mp[ok]) {
-            ans += n - mp[ok] + 1;
-        }
-    }
-    cout << ans;
     return 0;
 }
