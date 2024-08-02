dd3a4ea0bca19919aaaad1315c00c8e639a54ee9
chenyun-yoimiya <3129404199@qq.com>
2024-08-02 23:40:52 +0800

    1

--- 前缀和与差分/魔法封印.cpp
+++ 前缀和与差分/魔法封印.cpp
@@ -1,21 +1,62 @@
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	int a[10010] = { 0 };
+//	cin >> n;
+//	for (int i = 0; i < n; i++)
+//		cin >> a[i];
+//	int k = 0;
+//
+//	cin >> k;
+//	int x, y;
+//	while (k--)
+//	{
+//		cin >> x >> y;
+//		cout << upper_bound(a, a + n, y) - lower_bound(a, a + n, x) << '\n';
+//	}
+//	return 0;
+//}
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//    string s, str;
+//    getline(cin, s);
+//    getline(cin, str);
+//    while (s.find(str)!=-1) {
+//        s.erase(s.find(str), str.size());
+//    }
+//    cout << s;
+//    return 0;
+//}
+
 #include <iostream>
-#include <algorithm>
 using namespace std;
 int main()
 {
-	int n;
-	int a[10010] = { 0 };
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> a[i];
-	int k = 0;
-
-	cin >> k;
-	int x, y;
-	while (k--)
-	{
-		cin >> x >> y;
-		cout << upper_bound(a, a + n, y) - lower_bound(a, a + n, x) << '\n';
-	}
-	return 0;
+    int n, k;
+    string a[105];
+    cin >> n >> k;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < k; i++)
+    {
+        for (int j = 0; j + 1 < n - i; j++)
+        {
+            if (a[j] > a[j + 1])
+                swap(a[j + 1], a[j]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (i)
+            cout << '\n';
+        cout << a[i];
+    }
+    return 0;
 }
