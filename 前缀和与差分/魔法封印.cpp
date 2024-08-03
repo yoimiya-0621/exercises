1e88c8958a39b88e4495a3c42e7c6ab75cc66e4b
chenyun-yoimiya <3129404199@qq.com>
2024-08-03 21:48:59 +0800

    1

--- 前缀和与差分/魔法封印.cpp
+++ 前缀和与差分/魔法封印.cpp
@@ -35,28 +35,44 @@
 //    return 0;
 //}
 
-#include <iostream>
-using namespace std;
-int main()
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    int n, k;
+//    string a[105];
+//    cin >> n >> k;
+//    for (int i = 0; i < n; i++)
+//        cin >> a[i];
+//    for (int i = 0; i < k; i++)
+//    {
+//        for (int j = 0; j + 1 < n - i; j++)
+//        {
+//            if (a[j] > a[j + 1])
+//                swap(a[j + 1], a[j]);
+//        }
+//    }
+//    for (int i = 0; i < n; i++)
+//    {
+//        if (i)
+//            cout << '\n';
+//        cout << a[i];
+//    }
+//    return 0;
+//}
+
+ElementType Median(ElementType A[], int N)
 {
-    int n, k;
-    string a[105];
-    cin >> n >> k;
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-    for (int i = 0; i < k; i++)
-    {
-        for (int j = 0; j + 1 < n - i; j++)
+    for (int gap = N / 2; gap > 0; gap /= 2) {//Ï£¶ûÅÅÐò
+        for (int i = gap; i < N; i++)
         {
-            if (a[j] > a[j + 1])
-                swap(a[j + 1], a[j]);
+            for (int j = i - gap; j >= 0 && A[j] > A[j + gap]; j -= gap)
+            {
+                ElementType t = A[j + gap];
+                A[j + gap] = A[j];
+                A[j] = t;
+            }
         }
     }
-    for (int i = 0; i < n; i++)
-    {
-        if (i)
-            cout << '\n';
-        cout << a[i];
-    }
-    return 0;
+    return A[N / 2];
 }
